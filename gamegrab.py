f338f38187385c0403981b8379aeb0bbd63cd3b9
toddbryant <toddbryant@live.com>
2022-06-13 09:42:28 -0400

    add since parameter

--- gamegrab.py
+++ gamegrab.py
@@ -1,13 +1,14 @@
 """gamegrab
 
 Usage:
-  gamegrab.py [--blitz-only] [--outfile=OUTFILE] [--color=COLOR] [--show-eco-stats] USERNAME
+  gamegrab.py [--blitz-only] [--outfile=OUTFILE] [--color=COLOR] [--since=YYYYMM] [--show-eco-stats] USERNAME
   gamegrab.py (-h | --help)
 
 Options:
   --blitz-only          Only download blitz games.
   --outfile=OUTFILE     Name of outputfile (defaults to USERNAME.pgn).
   --color=COLOR         Download games of specific color.   
+  --since=YYYYMM        Only download games on or after given year and month.
   -h --help             Show this screen.
 
 Arguments:
@@ -24,10 +25,18 @@ def main(arguments):
     outfile = arguments['--outfile'] or '{0}.pgn'.format(user)
     blitz_only = arguments['--blitz-only']
     color = arguments['--color'].lower() if arguments['--color'] else None
+    since = arguments['--since'] if arguments['--since'] else None
+    if since:
+        from_year = int(since[:4])
+        from_month = int(since[5:6])
 
     with open(outfile, 'w') as f:
         urls = requests.get('https://api.chess.com/pub/player/{0}/games/archives'.format(user))
         for url in urls.json()['archives']:
+            if since:
+                url_year, url_month = map(int, url.split('/')[-2:])
+                if url_year < from_year or (url_year == from_year and url_month < from_month):
+                    continue
             print('Downloading {url}...'.format(url=url))
             games = requests.get(url)
             for game in games.json()['games']:
